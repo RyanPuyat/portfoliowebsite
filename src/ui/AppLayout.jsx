f32efa735f3e5eab3f04cbc2443f02693b64c05f
Ryan <ryanspuyat@gmail.com>
2025-10-16 23:16:21 +0900

    adding particles 2.0

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -5,7 +5,7 @@ import Particles from './Particles';
 export default function AppLayout() {
   return (
     // <div className="flex flex-col min-h-screen">
-    <div className="relative min-h-screen bg-white dark:bg-gray-950 overflow-hidden">
+    <div className="relative min-h-screen bg-white dark:bg-gray-950">
       <Particles id="particles" />
       <div className="relative z-10 flex flex-col min-h-screen">
         <MainNav />
